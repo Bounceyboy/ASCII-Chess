de6787f3e7851e4e4d1bbce0a0a0845e72dd074e
bboy <bboy@192.168.1.13>
2017-10-17 17:16:17 -0400

    First commit

---
+++ Chess08/src/chess/Chess.java
@@ -0,0 +1,17 @@
+package chess;
+
+public class Chess {
+	/* TODO:
+	 * - Make 2D array board
+	 * - Put each piece in the proper location
+	 */
+
+public static void Main(String[] args) {
+	/* TODO
+	 * - start chess game
+	 * - make board etc
+	 * - testing moves
+	 */
+}
+
+}
